a28ec8f236985875cbe212130cd1cb5981fa1eed
matterramun <Veradux4@protonmail.com>
2024-02-20 22:30:31 -0500

    Add keygen POC.
    First step in encrypting data stored on the app.
    Word array is generated on button press to represent key generation.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,23 @@
-import { StyleSheet } from 'react-native';
+import * as React from 'react';
+import CryptoES from 'crypto-es';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
+import { Button, Pressable, StyleSheet } from 'react-native';
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
+  const [keystring, setKeystring] = React.useState('test');
+  function keyGen () {
+    return CryptoES.lib.WordArray.random(256 / 8).toString();
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Home Tab</Text>
+      <Text>POC to Create and store a key</Text>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttontext} onPress={() => setKeystring(keyGen())}>Make a Key</Text>
+      </Pressable>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text>{keystring}</Text>
     </View>
   );
 }
@@ -24,8 +33,25 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   separator: {
-    marginVertical: 30,
+    marginVertical: 15,
     height: 1,
     width: '80%',
   },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'blue',
+    margin: 10,
+  },
+  buttontext: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  }
 });
